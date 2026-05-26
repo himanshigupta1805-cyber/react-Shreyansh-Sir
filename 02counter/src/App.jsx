3d2c3fc92599e10064f0f293263ab1cec11972f8
Shreyansh Jain <195452748+SHREYANSH2K25@users.noreply.github.com>
2026-05-26 06:55:06 +0000

    Refactor counter logic to use state for message handling and improve UI feedback

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -7,17 +7,24 @@ function App() {
   // using usestate hook - pass two agrument one var and other one calls that var 
   // useSTate mei koi bhi default value de skte instead of 15 like true false string kuch hhi
   let [counter, setCounter] = useState(15)
+  let [msg, setMsg] = useState("")
   //isne kaam let counter wala he kia h bas iska state change ho skta h on UI toh page mei jaha kahi bhi counter use hua h har jgh ek saath changes aajayenge 
   //let counter =15;
   const AddValue = ()=>{
     // counter = counter+1 aise nahi hoga
     setCounter(counter+1)
+    setMsg("")
   }
 
+  const PrintMsg = ()=>{
+    setMsg("Can not go to negatives")
+  }
   const RemoveValue = ()=>{
-    if(counter >0) 
-        setCounter(counter-1);
-    else document.querySelector('#para').innerHTML = "Cannot go to negatives"
+    if(counter >0) {
+      setCounter(counter-1);
+      setMsg("")
+    }
+    else setMsg("Cannot go to negatives")
   }
   return (
     <>
@@ -26,7 +33,7 @@ function App() {
       <button onClick={AddValue}>Increase counter : {counter}</button>
       <br />
       <button onClick={RemoveValue}>Decreaese counter : {counter}</button>
-      <p id="para"></p>
+      <p>{msg}</p>
     </>
   )
 }
